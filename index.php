bcaa6bcccace1fd1f7ac4e69d2cdd4d344f7fb9a
Chenxi Lin <lin.chenxi14@gmail.com>
2025-11-25 15:07:55 -0500

    Add styles

--- index.php
+++ index.php
@@ -21,37 +21,56 @@ include('includes/header.php');
     <hr>
 
     <h2 class="w3-blue w3-padding">Featured Sets</h2>
-    <p>TODO: 4 RANDOM SETS</p>
+    <div style="display:flex; justify-content: space-around;">
         <?php 
 
             $query= 'SELECT * FROM sets ORDER BY RAND() LIMIT 4';
             $result = mysqli_query($connect, $query);
 
-
+            
             while($randomSet = mysqli_fetch_assoc($result)) {
                 // var_dump($randomSet);
                 echo '
-                    <div>
-                        <img src="' . $randomSet["img_url"] . '" width="100" >
-                        <p>"' . $randomSet["name"] .'" </p>
-                        <p>"' . $randomSet["year"] .'" </p>
+                    
+                    <div style="width: calc(25% - 16px); box-sizing: border-box; display: flex; flex-direction: column;">
+                        <div class="w3-card-4 w3-margin-top w3-margin-bottom" style="max-width:100%; height: 100%; display: flex; flex-direction: column;">
+                            <header class="w3-container w3-blue">
+                                <h4 style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">' . $randomSet["name"] .'</h4>
+                            </header>
+                            <div class="w3-container w3-center w3-padding">
+                                <div style="position: relative; width: 100%; padding-top: 100%;">
+                                    <a href="http://localhost:8888/parts-v2/set.php?id=71858-1">
+                                        
+                                        <img src=' . $randomSet["img_url"] . ' alt="" style="max-width:80%; position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto;  max-width: 80%; max-height: 80%; object-fit: contain;">
+                                                                        
+                                    </a>
+                                </div>  
+                            </div>
+
+                            <!--
+                            <div class="w3-container w3-center w3-padding-16">
+                                <a href="http://localhost:8888/parts-v2/set.php?id=71858-1">Set Details</a>
+                            </div>
+                            -->
+                        </div>
                     </div>
                 ';
             }
 
 
         ?>
-    <hr>
+        </div>
+        <hr>
 
-    <h2 class="w3-indigo w3-padding">Featued Minifigures</h2>
-    <p>TODO: 4 RANDOM MINIFIGURES</p>
+        <h2 class="w3-indigo w3-padding">Featued Minifigures</h2>
+        <p>TODO: 4 RANDOM MINIFIGURES</p>
 
-    <hr>
+        <hr>
 
-    <h2 class="w3-purple w3-padding">Featued Parts</h2>
-    <p>TODO: 4 RANDOM PARTS</p>
+        <h2 class="w3-purple w3-padding">Featued Parts</h2>
+        <p>TODO: 4 RANDOM PARTS</p>
 
-    <hr>
+        <hr>
 
     <h2 class="w3-deep-orange w3-padding">Featured Categories</h2>
     <p>TODO: 4 RANDOM PART CATEGORIES</p>
