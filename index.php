8028ed2cd50c34272a671aa4f016e7dbd66d0526
Chenxi Lin <lin.chenxi14@gmail.com>
2025-12-01 20:16:13 -0500

    Add site url and match code structure

--- index.php
+++ index.php
@@ -27,51 +27,40 @@ include('includes/header.php');
             $query= 'SELECT * FROM sets ORDER BY RAND() LIMIT 4';
             $result = mysqli_query($connect, $query);
 
-            
-            while($randomSet = mysqli_fetch_assoc($result)) {
-                // var_dump($randomSet);
-                echo '
-                    
-                    <div style="width: calc(25% - 16px); box-sizing: border-box; display: flex; flex-direction: column;">
+        ?>
+
+        <?php while($randomSet = mysqli_fetch_assoc($result)): ?>
+            <div style="width: calc(25% - 16px); box-sizing: border-box; display: flex; flex-direction: column;">
                         <div class="w3-card-4 w3-margin-top w3-margin-bottom" style="max-width:100%; height: 100%; display: flex; flex-direction: column;">
                             <header class="w3-container w3-blue">
-                                <h4 style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">' . $randomSet["name"] .'</h4>
+                                <h4 style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"><?= $randomSet["name"] ?></h4>
                             </header>
                             <div class="w3-container w3-center w3-padding">
                                 <div style="position: relative; width: 100%; padding-top: 100%;">
-                                    <a href="http://localhost/parts-v2/set.php?id='.$randomSet['set_num'].'">
+                                    <a href="<?=SITE_URL?>/set.php?id=<?=$randomSet["set_num"]?>">
                                         
-                                        <img src=' . $randomSet["img_url"] . ' alt="" style="max-width:80%; position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto;  max-width: 80%; max-height: 80%; object-fit: contain;">
+                                        <img src="<?=$randomSet["img_url"]?>" alt="" style="max-width:80%; position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto;  max-width: 80%; max-height: 80%; object-fit: contain;">
                                          
-                                        <p class="w3-blue">' . $randomSet["year"] . '</p>
+                                        <p class="w3-blue"><?=$randomSet["year"]?></p>
                                     </a>
                                 </div>  
                             </div>
-
-                            <!--
-                            <div class="w3-container w3-center w3-padding-16">
-                                <a href="http://localhost:8888/parts-v2/set.php?id=71858-1">Set Details</a>
-                            </div>
-                            -->
                         </div>
                     </div>
-                ';
-            }
+        <?php endwhile; ?>
+    
+    </div>
+    <hr>
 
+    <h2 class="w3-indigo w3-padding">Featued Minifigures</h2>
+    <p>TODO: 4 RANDOM MINIFIGURES</p>
 
-        ?>
-        </div>
-        <hr>
+    <hr>
 
-        <h2 class="w3-indigo w3-padding">Featued Minifigures</h2>
-        <p>TODO: 4 RANDOM MINIFIGURES</p>
+    <h2 class="w3-purple w3-padding">Featued Parts</h2>
+    <p>TODO: 4 RANDOM PARTS</p>
 
-        <hr>
-
-        <h2 class="w3-purple w3-padding">Featued Parts</h2>
-        <p>TODO: 4 RANDOM PARTS</p>
-
-        <hr>
+    <hr>
 
     <h2 class="w3-deep-orange w3-padding">Featured Categories</h2>
     <p>TODO: 4 RANDOM PART CATEGORIES</p>
