695f7c1816fa490750237695c1d2604a6611cd05
Adam Thomas <thomasadam83@hotmail.com>
2025-12-09 10:57:12 -0500

    Updated sample env file

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title><?=PAGE_TITLE ? PAGE_TITLE.' | ' : ''?>BrickMMO Parts</title>
+    <title><?=PAGE_TITLE ? PAGE_TITLE.' | ' : ''?>Parts Directory</title>
 
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css">
     <link rel="stylesheet" href="https://cdn.brickmmo.com/exceptions@1.0.0/w3.css">
@@ -22,7 +22,7 @@
         <nav class="w3-row w3-section">
             <div class="w3-col s6 w3-left-align">
                 <a href="<?=SITE_URL?>/" style="font-size: 180%;">
-                    BrickMMO Parts
+                    Parts Directory
                 </a>
             </div>
             <div class="w3-col s6 w3-right-align">
