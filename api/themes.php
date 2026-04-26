bc92072654670a1ab2f31594738c659cf176b904
Dana Le <danale.ca@outlook.com>
2026-04-26 10:50:45 -0400

    Add themes API endpoint, clean up the code with error handling

--- api/themes.php
+++ api/themes.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Content-Type: application/json; charset=utf-8');
+header('Content-Type: application/json');
 
 include('includes/connect.php');
 include('includes/config.php');
@@ -8,14 +8,16 @@ include('includes/functions.php');
 
 $themes = array();
 
-$query = 'SELECT 
+$query = 'SELECT
     id, name, parent_id
-FROM themes
-ORDER BY name';
+    FROM themes
+    ORDER BY name';
 
 $result = mysqli_query($connect, $query);
-while ($theme = mysqli_fetch_assoc($result)) {
-    $themes[] = $theme;
+if (!$result) {
+    http_response_code(500);
+    echo json_encode(['error' => 'Database query failed']);
+    exit;
 }
 
 echo json_encode($themes);
